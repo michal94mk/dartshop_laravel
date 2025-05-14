3c711a4be7364bb754ae2ecefcb58d511e6f079e
Michał Kołodziejczyk <michal94mk@o2.pl>
2025-05-14 10:20:29 +0200

    feat: Add dartshop products with images Added real product data

--- database/seeders/ProductSeeder.php
+++ database/seeders/ProductSeeder.php
@@ -15,101 +15,136 @@ class ProductSeeder extends Seeder
     public function run()
     {
         $products = [
+            // Tarcze
             [
-                'name' => 'Phil Taylor Power 9Five Gen 8',
-                'description' => 'The latest generation of Phil Taylor\'s signature darts with Swiss points and 95% tungsten barrels.',
-                'price' => 129.99,
-                'category_id' => 1, // Darts
-                'brand_id' => 3, // Target
-                'image' => 'power9five.jpg'
-            ],
-            [
-                'name' => 'Winmau Blade 6 Triple Core',
-                'description' => 'The revolutionary triple core dartboard with reduced bounce-outs and improved durability.',
-                'price' => 79.99,
-                'category_id' => 2, // Dartboards
+                'name' => 'Winmau Blade 6 Triple Core Carbon',
+                'description' => 'Profesjonalna tarcza turniejowa z potrójnym rdzeniem i włóknem węglowym. Cieńsze druty redukują odbicia, a system Dual Core zapewnia większą trwałość.',
+                'price' => 89.95,
+                'category_id' => 2, // Dartboards/Tarcze
                 'brand_id' => 4, // Winmau
-                'image' => 'blade6.jpg'
+                'image' => 'winmau-blade-6-triple-core-carbon.jpg'
             ],
             [
-                'name' => 'Red Dragon Razor Edge ZX-3',
-                'description' => 'Precision-engineered steel-tip darts with 90% tungsten barrels and Nitrotech shafts.',
-                'price' => 69.99,
-                'category_id' => 1, // Darts
-                'brand_id' => 5, // Red Dragon
-                'image' => 'razeredge.jpg'
+                'name' => 'Target Aspar',
+                'description' => 'Wysokiej jakości tarcza turniejowa z cienkimi drutami i zwiększoną powierzchnią wbijania.',
+                'price' => 44.95,
+                'category_id' => 2, // Dartboards/Tarcze
+                'brand_id' => 3, // Target
+                'image' => 'target-aspar.jpg'
             ],
             [
-                'name' => 'Unicorn Eclipse HD2 Pro Dartboard',
-                'description' => 'Tournament-quality dartboard with high-definition number ring and ultra-thin wiring.',
-                'price' => 64.99,
-                'category_id' => 2, // Dartboards
+                'name' => 'Unicorn Eclipse Ultra',
+                'description' => 'Profesjonalna tarcza turniejowa z systemem Eclipse HD. Cienkie druty i wyraźny kontrast kolorów dla lepszej widoczności.',
+                'price' => 59.95,
+                'category_id' => 2, // Dartboards/Tarcze
                 'brand_id' => 1, // Unicorn
-                'image' => 'eclipsehd2.jpg'
+                'image' => 'unicorn-eclipse-ultra.jpg'
+            ],
+            
+            // Lotki Steel
+            [
+                'name' => 'Target Vapor 8 Black',
+                'description' => 'Lotki z 80% wolframu z charakterystycznym czarnym wykończeniem i unikalnym systemem uchwytu. Dostępne w wersjach z kolorowymi akcentami.',
+                'price' => 56.95,
+                'category_id' => 1, // Darts/Lotki
+                'brand_id' => 3, // Target
+                'image' => 'target-vapor-8-black.jpg'
             ],
             [
-                'name' => 'Harrows Ace Case',
-                'description' => 'Premium dart case with secure storage for up to three sets of darts and accessories.',
-                'price' => 19.99,
-                'category_id' => 5, // Cases
+                'name' => 'Red Dragon Amberjack 25g',
+                'description' => 'Profesjonalne lotki z 90% wolframu z charakterystycznym pomarańczowo-czarnym wzorem grippowym. Wyjątkowa precyzja i kontrola.',
+                'price' => 37.95,
+                'category_id' => 1, // Darts/Lotki
+                'brand_id' => 5, // Red Dragon
+                'image' => 'red-dragon-amberjack-25g.jpg'
+            ],
+            [
+                'name' => 'Winmau Blackout',
+                'description' => 'Eleganckie czarne lotki z 90% wolframu z matowym wykończeniem i precyzyjnym systemem gripowym.',
+                'price' => 49.95,
+                'category_id' => 1, // Darts/Lotki
+                'brand_id' => 4, // Winmau
+                'image' => 'winmau-blackout.jpg'
+            ],
+            
+            // Lotki Soft
+            [
+                'name' => 'Target Bolide 01',
+                'description' => 'Lotki softip z 90% wolframu zaprojektowane dla maksymalnej kontroli i precyzji.',
+                'price' => 59.95,
+                'category_id' => 1, // Darts/Lotki 
+                'brand_id' => 3, // Target
+                'image' => 'target-bolide-01.jpg'
+            ],
+            [
+                'name' => 'Harrows Spina',
+                'description' => 'Lotki softip z 90% wolframu z charakterystycznym spiralnym wzorem gripowym.',
+                'price' => 44.95,
+                'category_id' => 1, // Darts/Lotki
                 'brand_id' => 2, // Harrows
-                'image' => 'acecase.jpg'
+                'image' => 'harrows-spina.jpg'
             ],
+            
+            // Akcesoria
             [
-                'name' => 'Target Vision Pro Ultra Flights',
-                'description' => 'Aerodynamically engineered flights for improved stability and reduced drag.',
-                'price' => 4.99,
-                'category_id' => 6, // Flights
+                'name' => 'Target Corona Vision Lighting System',
+                'description' => 'Profesjonalny system oświetlenia tarczy eliminujący cienie i poprawiający widoczność.',
+                'price' => 79.95,
+                'category_id' => 3, // Accessories/Akcesoria
                 'brand_id' => 3, // Target
-                'image' => 'visionpro.jpg'
+                'image' => 'target-corona-vision.jpg'
             ],
             [
-                'name' => 'Shot Tribal Weapon Series',
-                'description' => 'Distinctive darts with unique tribal engravings and 90% tungsten construction.',
-                'price' => 89.99,
-                'category_id' => 1, // Darts
-                'brand_id' => 6, // Shot Darts
-                'image' => 'tribalweapon.jpg'
+                'name' => 'Winmau Simon Whitlock Case',
+                'description' => 'Profesjonalne etui na lotki z twardą obudową, sygnowane przez Simona Whitlocka.',
+                'price' => 19.95,
+                'category_id' => 5, // Cases/Etui
+                'brand_id' => 4, // Winmau
+                'image' => 'winmau-simon-whitlock-case.jpg'
             ],
+            
+            // Lotki
             [
-                'name' => 'Mission Quad Lock Shafts',
-                'description' => 'Revolutionary shaft design with four-point locking system to prevent loosening.',
-                'price' => 5.99,
-                'category_id' => 7, // Shafts
+                'name' => 'Mission Snakebite World Champion Edition',
+                'description' => 'Lotki z 90% wolframu sygnowane przez mistrza świata PDC Petera Wrighta, z charakterystycznym wzorem wężowej skóry.',
+                'price' => 69.95,
+                'category_id' => 1, // Darts/Lotki
                 'brand_id' => 7, // Mission
-                'image' => 'quadlock.jpg'
+                'image' => 'mission-snakebite-champion.jpg'
             ],
+            
+            // Shafty
             [
-                'name' => 'Bull\'s Scorer Dartboard Cabinet',
-                'description' => 'Elegant wooden cabinet with scoreboard and storage for darts and accessories.',
-                'price' => 109.99,
-                'category_id' => 10, // Cabinet Sets
-                'brand_id' => 8, // Bull's
-                'image' => 'scorer.jpg'
-            ],
-            [
-                'name' => 'Designa Dark Thunder Darts',
-                'description' => 'Budget-friendly 90% tungsten darts with black titanium coating.',
-                'price' => 24.99,
-                'category_id' => 1, // Darts
-                'brand_id' => 9, // Designa
-                'image' => 'darkthunder.jpg'
-            ],
-            [
-                'name' => 'One80 Gladiator 3 Darts',
-                'description' => 'Professional-grade darts with revolutionary grip pattern and 95% tungsten barrels.',
-                'price' => 94.99,
-                'category_id' => 1, // Darts
-                'brand_id' => 10, // One80
-                'image' => 'gladiator3.jpg'
-            ],
-            [
-                'name' => 'Target Corona LED Lighting System',
-                'description' => 'Professional dartboard lighting system eliminating shadows for better visibility.',
-                'price' => 59.99,
-                'category_id' => 3, // Accessories
+                'name' => 'Target Pro Grip Shafts',
+                'description' => 'Profesjonalne shafty z unikalnym systemem Pro Grip zapobiegającym luzowaniu się podczas gry.',
+                'price' => 5.95,
+                'category_id' => 7, // Shafts/Shafty
                 'brand_id' => 3, // Target
-                'image' => 'corona.jpg'
+                'image' => 'target-pro-grip-shafts.jpg'
+            ],
+            [
+                'name' => 'Designa DSX Flights',
+                'description' => 'Wytrzymałe i aerodynamiczne lotki dostępne w różnych kształtach i wzorach.',
+                'price' => 2.95,
+                'category_id' => 6, // Flights/Piórka
+                'brand_id' => 9, // Designa
+                'image' => 'designa-dsx-flights.jpg'
+            ],
+            [
+                'name' => 'Bull\'s Dartmate Plus Scorepad',
+                'description' => 'Profesjonalna tablica do zapisywania wyników z magnetycznym markerem.',
+                'price' => 24.95,
+                'category_id' => 3, // Accessories/Akcesoria
+                'brand_id' => 8, // Bull's
+                'image' => 'bulls-dartmate-plus.jpg'
+            ],
+            [
+                'name' => 'One80 Reflex Points',
+                'description' => 'Profesjonalne groty steel o zwiększonej wytrzymałości i zmniejszonej częstotliwości odbić.',
+                'price' => 9.95,
+                'category_id' => 3, // Accessories/Akcesoria
+                'brand_id' => 10, // One80
+                'image' => 'one80-reflex-points.jpg'
             ]
         ];
 
