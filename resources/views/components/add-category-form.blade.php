5e173768afe0e87091c391afaf0aa6421c7f8896
Michał Kołodziejczyk <michal94mk@o2.pl>
2025-05-14 09:17:08 +0200

    refactor: clean up views directory structure and remove unused components

--- resources/views/components/add-category-form.blade.php
+++
@@ -1,8 +0,0 @@
-<form action="{{ route('admin.categories.store') }}" method="POST">
-    @csrf
-    <div class="form-group">
-        <label for="category_name">Name:</label><br>
-        <input type="text" name="name" id="category_name" class="form-control" required><br>
-    </div>
-    <button type="submit" class="btn btn-primary">Save</button>
-</form>
--- resources/views/components/edit-user-form.blade.php
+++
@@ -1,9 +0,0 @@
-<form action="{{ route('admin.users.update', ['user' => $user->id]) }}" method="POST">
-    @csrf
-    @method('PUT')
-    <div class="form-group">
-        <label for="name">Name:</label><br>
-        <input type="text" name="name" id="name" class="form-control" value="{{ $user->name }}" required><br>
-    </div>
-    <button type="submit" class="btn btn-primary">Save changes</button>
-</form>
